f0757691f80a2882cc145e9273fc0ae27366099d
StefaniaVitali <stefy.egypt@hotmail.it>
2024-03-06 21:30:28 +0100

    random value for casual password and adding alert message

--- js/app.js
+++ js/app.js
@@ -9,4 +9,13 @@ console.log(userSurname);
 const userColor = prompt('Qual è il tuo colore preferito?'); //string | null
 console.log(userColor);
 
-console.log(userName + userSurname + userColor + 21);
+console.log(userName + userSurname + userColor + 21); //string 
+
+const maxValue = 99; //number
+let randomValue = Math.floor((Math.random() * maxValue) + 1); //number
+console.log(randomValue) //number
+
+console.log(userName + userSurname + userColor + randomValue); //string 
+
+let passwordGenerated = (userName + userSurname + userColor + randomValue); //string
+alert ('La tua password generata è: ' + passwordGenerated ); //string
