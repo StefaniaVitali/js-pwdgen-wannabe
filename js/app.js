92a90ba5c5905c8cbc6a47b97ad9b80e3cc31695
StefaniaVitali <stefy.egypt@hotmail.it>
2024-03-06 21:05:36 +0100

    password generatore with my chosen number

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log ('Ciao dal file js')
+console.log ('Ciao dal file js'); //control js file correctly linked
+
+const userName = prompt ('Come ti chiami?'); //string | null
+console.log(userName);
+
+const userSurname = prompt ('Qual è il tuo cognome?'); //string | null
+console.log(userSurname);
+
+const userColor = prompt('Qual è il tuo colore preferito?'); //string | null
+console.log(userColor);
+
+console.log(userName + userSurname + userColor + 21);
